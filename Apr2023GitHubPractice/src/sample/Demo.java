78005e1758a1d49809d21c3ca7c04063883614ae
Aspire <87691699+atipune@users.noreply.github.com>
2023-04-15 08:10:47 +0530

    Update Demo.java

--- Apr2023GitHubPractice/src/sample/Demo.java
+++ Apr2023GitHubPractice/src/sample/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hi, this is my first github push");
 		System.out.println("Hi");
+		System.out.println("Change by another person");
 	}
 }
